df6c63eecb2202bb4de57fedfbe70c75a9c2ff95
PanditUtkarsh <panditutkarsh555@gmail.com>
2022-08-11 01:56:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { HiMenu } from "react-icons/hi";
+import { HiX } from "react-icons/hi";
+import { useState } from 'react';
+
 
 function App() {
+
+  const [menu,setmenu] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+       <div className='navbar'>
+
+            <div className='logo'>LOGO</div>
+
+
+              <ul className={menu ? 'navbar_mobile_css' :  'navbar_ul'}>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Movies</a></li>
+                <li><button className='button_signup'>Signup</button></li>
+                <li><button className='button_login'>Login</button></li>
+              </ul>
+              <div className='menu_icon' onClick={() => setmenu(!menu)}>
+                    {menu ? <HiX size={46} /> :<HiMenu size={46} />}
+              </div>
+
+       </div>
+
     </div>
   );
 }
+
 
 export default App;
